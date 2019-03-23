742b27b22d7c7c7f253e44db18873ed8dffbb855
Woojong Kim <woojong92@gmail.com>
2019-03-23 23:06:33 +0900

    add ToDOList & weather API

--- todo.js
+++ todo.js
@@ -4,9 +4,22 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
-const toDos = [];
 
-function saveToDo(){
+
+let toDos = [];
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    })
+    toDos = cleanToDos;
+    saveToDos();
+}
+
+function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
@@ -17,6 +30,8 @@ function paintToDo(text) {
     const newId = toDos.length + 1;
     
     delBtn.innerHTML= "X";
+    delBtn.addEventListener("click", deleteToDo);
+    
     span.innerText = text;
     
     li.appendChild(span);
@@ -29,7 +44,7 @@ function paintToDo(text) {
         id : newId
     };
     toDos.push(toDoObj);
-    saveToDo();
+    saveToDos();
 }
 
 function handleSubmit(event) {
---
+++ weather.js
@@ -0,0 +1,40 @@
+const COORDS ="coords";
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+    //console.log(position);
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude, 
+        longitude
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+    console.log("err");
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+    const loadedCords = localStorage.getItem(COORDS);
+    if( loadedCords === null ) {
+        askForCoords();
+    }else{
+        //getWeather
+    }
+
+}
+
+function init() {
+    loadCoords();
+}
+
+init();
