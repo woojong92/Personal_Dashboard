c542310f9c005c6a139edbe567341cd2ef196520
Woojong Kim <woojong92@gmail.com>
2019-04-03 10:54:38 +0900

    .

--- weather.js
+++ weather.js
@@ -1,4 +1,22 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "550516674abc77478478b549c21fe570";
 const COORDS ="coords";
+
+function getWeather(lat, log){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`)
+    .then(function(res) {
+        //console.log(res.json());
+        return res.json();
+    })
+    .then(function(json) {
+        console.log(json);
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`
+    });
+}
+
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,6 +31,7 @@ function handleGeoSucces(position) {
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -28,9 +47,10 @@ function loadCoords() {
     if( loadedCords === null ) {
         askForCoords();
     }else{
-        //getWeather
+        const parseCoods = JSON.parse(loadedCords);
+        //console.log(parseCoods);
+        getWeather(parseCoods.latitude, parseCoods.longitude);
     }
-
 }
 
 function init() {
